2113505f5229efaa018ea591033a5ae46168ca77
Sandeep Kumar Verma <sandeep.kv@fgtech.us>
2016-07-07 17:39:39 +0530

    Configuring Spring

--- mspring/WebContent/index.jsp
+++ mspring/WebContent/index.jsp
@@ -8,5 +8,6 @@
 </head>
 <body>
 Welcome Sandy.
+${content}
 </body>
 </html>
---
+++ mspring/src/com/sm/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.sm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public ModelAndView welcomeMsg(Model model) {
+		model.addAttribute("content", "Welcome to My Site.");
+		return new ModelAndView("index");
+	}
+}
